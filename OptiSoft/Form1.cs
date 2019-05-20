b0a2d9c09d7fbe41eb80aa6789ec618ab54c0c06
triest <you@example.com>
2019-05-20 21:48:13 +0300

    add connection

--- OptiSoft/Form1.cs
+++ OptiSoft/Form1.cs
@@ -16,5 +16,11 @@ namespace OptiSoft
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SQL sql = new SQL();
+            sql.GetDBConnection();
+        }
     }
 }
---
+++ OptiSoft/SQL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace OptiSoft
+{
+
+   
+    class SQL
+    {
+        public static object DBSQLServerUtils { get; private set; }
+
+        public SqlConnection con;
+        //To Handle connection related activities
+        private void connection()
+        {
+            string constr = "Data Source=MACHINE-VOIV7EH\\SQLEXPRESS; Initial Catalog = OptiSoft; Persist Security Info = False;Integrated Security=True;";
+            con = new SqlConnection(constr);
+        }
+
+        public void GetDBConnection()
+        {
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = "Data Source=MACHINE-VOIV7EH\\SQLEXPRESS; Initial Catalog = geolog; Persist Security Info = False;Integrated Security=True;";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            Console.Write("connect");
+            cnn.Close();
+        }
+    }
+}
